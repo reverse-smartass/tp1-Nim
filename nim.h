3a1567265fa4d022e5ff18c665c027cfd7e1c6eb
Pokefaz <144361985+Pokefaz@users.noreply.github.com>
2023-10-17 10:05:42 -0400

    Add files via upload

---
+++ nim.h
@@ -0,0 +1,27 @@
+/*******************************************************************************/
+/*  NIM.H                                                                      */
+/*  Ce module contient les fonctions necéssaires à l'implémantation du jeu NIM */
+/*******************************************************************************/
+#ifndef MANIP_PLATEAU_H_
+#define MANIP_PLATEAU_H_
+
+#include "codage_numerique.h"
+#include "m_distributions.h"
+
+#define PLATEAU_MAX_COLONNES 20		//Nombre maximal de colonnes sur le plateau
+#define PLATEAU_MAX_PIECES 35		//Nombre maximal de pièces par colonne
+#define TRUE   1
+#define FALSE  0
+
+/*******************************************************************************/
+/*                   DÉCLARATION DES FONCTIONS PUBLIQUES                       */
+/*******************************************************************************/
+
+
+
+
+
+
+
+
+#endif
