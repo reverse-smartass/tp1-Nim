ee7c1ef3fbede43fe6ca94e94f9a20840e64330d
reverse-smartass <107412957+reverse-smartass@users.noreply.github.com>
2023-10-17 10:21:07 -0400

    m

--- nim.c
+++ nim.c
@@ -1,9 +1,17 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
+#include "nim.h"
 
-int main()
-{
-    printf("Hello World!\n");
-    return 0;
+int lire_entier(int min, int max){
+
+    int i;
+
+    printf("Saisir une valeur entière entre 1 et 4: ");
+    scanf("%i", &i);
+    while(i > 4 | i < 1){
+        printf("Saisir une valeur entière entre 1 et 4: ");
+        scanf("%i", &i);
+    }
+    return i;
 }
