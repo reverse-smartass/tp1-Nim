ba318ff2f83c7fdae8a49ee93763fd34ce1ec61e
reverse-smartass <107412957+reverse-smartass@users.noreply.github.com>
2023-10-19 15:32:58 -0400

    n

--- nim.c
+++ nim.c
@@ -1,7 +1,11 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
-#include "nim.h"
+//#include "nim.h"
+
+#define PLATEAU_MAX_PIECES 35
+#define PLATEAU_MIN_COLONNES 2
+#define PLATEAU_MAX_COLONNES 20	
 
 int lire_entier(int min, int max){
 
@@ -9,9 +13,38 @@ int lire_entier(int min, int max){
 
     printf("Saisir une valeur entière entre 1 et 4: ");
     scanf("%i", &i);
-    while(i > 4 || i < 1){
+    while(i > 4 | i < 1){
         printf("Saisir une valeur entière entre 1 et 4: ");
         scanf("%i", &i);
     }
     return i;
+}
+
+void plateau_init(int plateau[], int nb_colonnes)
+{
+    int i;
+
+    for (i = 0; i < nb_colonnes; i++)
+    {
+        plateau[i] = (random() / ((double) RAND_MAX + 1)) * (PLATEAU_MAX_PIECES + 1);
+    }
+}
+
+void plateau_supprimer_colonne(int plateau[], int nb_colonnes, int col_a_supprimer)
+{
+    int i;
+
+    for (i = col_a_supprimer; i < nb_colonnes - 1; i++)
+    {
+        plateau[i] = plateau[i + 1];
+        plateau[i + 1] = 0;
+    }
+}
+
+void nim_choix_ia_aleatoire(const int plateau[], int nb_colonnes, int * choix_colonne, int * choix_nb_pieces)
+{
+    int i;
+
+    * choix_colonne = (random() / ((double) RAND_MAX + 1)) * (PLATEAU_MAX_COLONNES + 1);
+    * choix_nb_pieces = (random() / ((double) RAND_MAX + 1)) * (PLATEAU_MAX_PIECES + 1);
 }
---
+++ nim.h
@@ -0,0 +1,36 @@
+/*******************************************************************************/
+/*  NIM.H                                                                      */
+/*  Ce module contient les fonctions nec�ssaires � l'impl�mantation du jeu NIM */
+/*******************************************************************************/
+#ifndef MANIP_PLATEAU_H_
+#define MANIP_PLATEAU_H_
+
+//#include "codage_numerique.h"
+//#include "m_distributions.h"
+
+#define PLATEAU_MAX_COLONNES 20		//Nombre maximal de colonnes sur le plateau
+#define PLATEAU_MAX_PIECES 35		//Nombre maximal de pi�ces par colonne
+#define TRUE   1
+#define FALSE  0
+
+/*******************************************************************************/
+/*                   D�CLARATION DES FONCTIONS PUBLIQUES                       */
+/*******************************************************************************/
+
+int lire_entier(int min, int max);
+int nim_jouer_tour(int plateau[], int nb_colonnes, int colonne, int nb_pieces);
+void plateau_init(int plateau[], int nb_colonnes);
+void plateau_supprimer_colonne(int plateau[], int nb_colonnes, int col_a_supprimer);
+int plateau_defragmenter(int plateau[], int nb_colonnes);
+void nim_choix_ia_aleatoire(const int plateau[], int nb_colonnes, 
+                            int * choix_colonne, int * choix_nb_pieces);
+void nim_choix_ia(const int plateau[], int nb_colonnes, int niveau, 
+                  int * choix_colonne, int * choix_nb_pieces); 
+
+
+
+
+
+
+
+#endif
