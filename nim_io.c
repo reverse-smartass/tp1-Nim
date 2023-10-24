17fd3b3e2ddd2689485712ebacfd7195356fe3a4
reverse-smartass <107412957+reverse-smartass@users.noreply.github.com>
2023-10-24 10:22:02 -0400

    y

--- nim_io.c
+++ nim_io.c
@@ -21,7 +21,7 @@ int lire_entier(int min, int max){
 void plateau_afficher(const int plateau[], int nb_colonnes, int col_select){
 
     gotoxy(0,5);
-
+    //print axe Y
     for(int k = PLATEAU_MAX_COLONNES; k>=1; k--){
         printf("%i\n\n", k);
     }
@@ -29,14 +29,16 @@ void plateau_afficher(const int plateau[], int nb_colonnes, int col_select){
     int posy = PLATEAU_MAX_COLONNES + 5;
     int posxtmp= 2;
 
+    //print axe X 
+    int posY = posy+4;
     for(int l = 0; l < nb_colonnes; l++){
-        gotoxy(posxtmp, posy+2);
+        gotoxy(posxtmp, posY);
         printf("%i", l);
-        posxtmp +=2;
+        posxtmp +=3;
     }
-
+    //print colonnes de *
     for(int i = 0; i < nb_colonnes; i++){
-        int posx = i*2+2;
+        int posx = i*3+2;
         gotoxy(posx, posy);
         for(int j = 0; j < plateau[i]; i++){
             printf("*");
@@ -44,6 +46,17 @@ void plateau_afficher(const int plateau[], int nb_colonnes, int col_select){
         }
     }
 
+    //positionner a la colonne choisie
+    int YColonneChoisie = 2+col_select*3;
+    gotoxy(YColonneChoisie, posY);
+    textbackground(RED);
+    gotoxy(YColonneChoisie, posy);
+    
+    int j;
+    for(j = 1; j <= plateau[col_select]; j++){
+        textbackground(RED);
+        gotoxy(YColonneChoisie,posy-j);
+    }
 
 
 }
@@ -52,7 +65,7 @@ static int choisir_colonne(int plateau[], int nb_colonnes)
 {
     int colonne = 0;
     unsigned char input;
-
+    int choix = 0;
     while(input != 13){
         input = getch();
 
@@ -62,10 +75,12 @@ static int choisir_colonne(int plateau[], int nb_colonnes)
             switch (input)
             {
             case (75):
-                /* code */
+                plateau_afficher(plateau, nb_colonnes, (choix < 0 ? nb_colonnes-1 : choix + 1));
+                choix < 0 ? choix = nb_colonnes-1 : choix++;
                 break;
             case (77):
-                /* code */
+                plateau_afficher(plateau, nb_colonnes, (choix > nb_colonnes-1 ? 0 : choix + 1));
+                choix > nb_colonnes-1 ? choix = 0 : choix++;
                 break;
             default:
                 break;
