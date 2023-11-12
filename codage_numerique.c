c76a284b9fa7281c01cffb9c5f5a53d9d099ff26
Fazil Boudjerada <fazil.boudjerada.1@ens.etsmtl.ca>
2023-11-12 18:23:04 -0500

    sdfgh

--- codage_numerique.c
+++ codage_numerique.c
@@ -12,7 +12,7 @@ int inverser_tab_bits(int tab_bits[], int nb_bits){
         tab_bits[i] = tab_bits[finalvalue-i];
         tab_bits[finalvalue-i] = temp;
     }
-    return nb_bits>8? 0 : 1;
+    return nb_bits>CODAGE_NB_BITS ? 0 : 1;
 }
 
 // Fonction qui permet la conversion de nombres décimaux en nombres binaires
@@ -21,11 +21,58 @@ int codage_dec2bin(int nombre, int resultat[]){
     int i;
     int holder = nombre;
     int powe;
+    int digit = 0;
 
-    // Pour la valeur décimale, division par 2 et sauvegarde de son modulo jusqu'à une valeur nulle
-    for(i = CODAGE_NB_BITS-1; i > 0; i--){
-        powe = pow(2,i); 
-        resultat[i] = holder/pow(2,i);
-        holder = nombre-powe;
+    // Tant que la valeur est positive, on divise par deux et on augmente le compteur de digits
+    while (nombre > 0) {
+        nombre /= 2;
+        digit++;
     }
+
+    if (digit > CODAGE_NB_BITS) {
+        return 0;
+    }
+
+    for (i = 0; i < CODAGE_NB_BITS; i++) {
+        resultat[i] = 0;
+    }
+
+    for (i = CODAGE_NB_BITS - digit; i < CODAGE_NB_BITS; i++) {
+
+        powe = pow(2, CODAGE_NB_BITS - 1 - i);
+        resultat[i] = holder / powe;
+        if (resultat[i] == 1) {
+            holder -= powe;
+        }
+    }
+    
+    return digit;
+}
+
+// Fonction qui permet l'affichage des bits dans le bonne ordre (valeur binaire switcher)
+void afficher_tab_bits(const int tab_bits[], int nb_bits) {
+    int i = 0;
+
+    for (i = 0; i < nb_bits; i++) {
+        printf("%i ", tab_bits[i]);
+    }
+    printf("\n");
+}
+// Fonction codage_dec2bin utilisant tab_bits[] pour stocker les valeurs binaires
+int codage_bin2dec(const int tab_bits[]) {
+
+    int i = 0;
+    int j;
+    int somme = 0;
+
+    while (tab_bits[i] == 0) {
+        i++;
+    }
+
+    // Boucle qui permet de calculer la somme des bits
+    for (j = i; j < CODAGE_NB_BITS; j++) {
+        somme += tab_bits[j] * pow(2, (CODAGE_NB_BITS - 1) - j);
+        //printf("%i ", somme);
+    }
+    return somme;
 }
